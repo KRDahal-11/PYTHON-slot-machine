9ec3e5a8ec1572f2c33b31710554506975030eef
KRDahal-11 <dahalkrish39@gmail.com>
2025-05-21 21:31:52 +0545

    version 1 : Before slot machine

--- main.py
+++ main.py
@@ -9,34 +9,56 @@ def depo():
         if amount.isdigit():
             amount = int(amount)
             if amount >= 20:
-                break
+                return amount
             else:
                 print("Amount must be greater than or equal to 20$")
         else:
             print("Please Enter a NUMBER!")
-    return amount
+    
 
 def get_no_lines():
     while True:
         lines = input ("Enter Number of lines you want to bet on : (1-" +str(MAX_LINES)+")? ")
-        if lines.isdigit():
+        if lines.isdigit(): #to check if user input is a digit
             lines = int(lines)
             if 1 <= lines <= MAX_LINES:
-                break
+                return lines
             else:
                 print("Lines Should be valid (1-"+ str(MAX_LINES)+")")
         else:
             print("Please Enter a NUMBER!")
-    return lines
+    
 
-
+def get_bet():
+     while True:
+        amount = input ("How much bet on EACH LINE $:")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET<= amount <= MAX_BET:
+                return amount
+            else:
+                print(f"Amount must be between ${MIN_BET} & ${MAX_BET}")
+        else:
+            print("Please Enter a NUMBER!")
     
 
 
 def main():
     balance = depo()
     lines = get_no_lines()
-    print(balance ,  lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"Your total bet is greater than BALANCE: {balance}")
+        else:
+            break
+    print(f"You are betting ${total_bet} in total")
+    balance = balance - total_bet
+    print(f"Balance remaining: ${balance}")
+
+
 
 
 
